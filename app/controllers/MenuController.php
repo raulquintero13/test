8467fdd05d762d8cdba3c0732cbc79aa9b2eb555
Raul Quintero <raulquintero13@gmail.com>
2018-03-21 23:12:57 -0700

    - new info

--- app/controllers/MenuController.php
+++ app/controllers/MenuController.php
@@ -68,7 +68,60 @@ class MenuController
         self::$args = $args;
 
         $menu = new Menu(DbConfig::$default);
-        $info = $_SERVER;
+        $info = "";
+
+
+        $indicesServer = array(
+            'PHP_SELF',
+            'argv',
+            'argc',
+            'GATEWAY_INTERFACE',
+            'SERVER_ADDR',
+            'SERVER_NAME',
+            'SERVER_SOFTWARE',
+            'SERVER_PROTOCOL',
+            'REQUEST_METHOD',
+            'REQUEST_TIME',
+            'REQUEST_TIME_FLOAT',
+            'QUERY_STRING',
+            'DOCUMENT_ROOT',
+            'HTTP_ACCEPT',
+            'HTTP_ACCEPT_CHARSET',
+            'HTTP_ACCEPT_ENCODING',
+            'HTTP_ACCEPT_LANGUAGE',
+            'HTTP_CONNECTION',
+            'HTTP_HOST',
+            'HTTP_REFERER',
+            'HTTP_USER_AGENT',
+            'HTTPS',
+            'REMOTE_ADDR',
+            'REMOTE_HOST',
+            'REMOTE_PORT',
+            'REMOTE_USER',
+            'REDIRECT_REMOTE_USER',
+            'SCRIPT_FILENAME',
+            'SERVER_ADMIN',
+            'SERVER_PORT',
+            'SERVER_SIGNATURE',
+            'PATH_TRANSLATED',
+            'SCRIPT_NAME',
+            'REQUEST_URI',
+            'PHP_AUTH_DIGEST',
+            'PHP_AUTH_USER',
+            'PHP_AUTH_PW',
+            'AUTH_TYPE',
+            'PATH_INFO',
+            'ORIG_PATH_INFO'
+        );
+
+        foreach ($indicesServer as $arg) {
+            if (isset($_SERVER[$arg])) {
+                $info [ $arg ] = $_SERVER[$arg] ;
+            } else {
+                echo $info[ $arg ] = "-";
+            }
+        }
+
 
 
         return $response->withJSON(
