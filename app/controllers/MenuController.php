95d9fc52c1383f04facea43f3b587deae8cddf62
Raul Quintero <raulquintero13@gmail.com>
2018-03-22 22:33:18 -0700

    + <pre>

--- app/controllers/MenuController.php
+++ app/controllers/MenuController.php
@@ -140,12 +140,13 @@ class MenuController
         // if ($_SERVER['REQUEST_METHOD'] === 'POST'
         //         && $_REQUEST['thing'] === PRIVATE_KEY)
         {
-        echo "Update:<br><br>\n";
+        echo "<pre>Update:<br><br>\n";
 
             echo shell_exec("/usr/bin/git pull");
             echo "<br><br>";
             echo "log<br><Br>\n";
             echo shell_exec("/usr/bin/git log --oneline");
+            echo "</pre>";"
         }
         die;
     }
